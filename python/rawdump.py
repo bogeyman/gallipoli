5ed5b4550ac0273e4b05a40042a5836cf666f7c0
Tim Gregory <gamma@valar.de>
2017-09-27 17:55:37 +0200

    Modifizierte Version fuer die Gamma 10

--- python/rawdump.py
+++ python/rawdump.py
@@ -11,12 +11,15 @@ import serial, binascii, sys
 # parity PARITY_NONE, PARITY_EVEN, PARITY_ODD PARITY_MARK, PARITY_SPACE
 # stopbits STOPBITS_ONE, STOPBITS_ONE_POINT_FIVE, STOPBITS_TWO
 serialDevice = '/dev/ttyUSB0'
-port = serial.Serial(serialDevice, baudrate=9600, timeout=60.0, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE, parity=serial.PARITY_NONE, xonxoff=False, rtscts=False, dsrdtr=False)
 
 while True:
-    rcv = port.read(40)
-    hex = binascii.hexlify(rcv)
-    # print hex + " " + repr(rcv)
-    print hex
-    sys.stderr.flush()
-    sys.stdout.flush()
+    try:
+	port = serial.Serial(serialDevice, baudrate=9600, timeout=60.0, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE, parity=serial.PARITY_NONE, xonxoff=False, rtscts=False, dsrdtr=False)
+        rcv = port.read(40)
+        hex = binascii.hexlify(rcv)
+        # print hex + " " + repr(rcv)
+        print hex
+        sys.stderr.flush()
+        sys.stdout.flush()
+    except Exception as e:
+        pass
